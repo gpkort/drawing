0e20cc63465a51ada32ecc98bde1caa2125209d1
greg k <gpkort@gmail.com>
2023-05-05 08:38:40 -0600

    new configs

---
+++ new_webpack.config.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const fs = require("fs");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const appDirectory = fs.realpathSync(process.cwd());
+
+module.exports = {
+    devtool: "source-map",
+    entry: path.resolve(appDirectory, "src/app.ts"), //path to the main .ts file
+    output: {
+        filename: "js/bundleName.js", //name for the js file that is created/compiled in memory
+        clean: true,
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
+    devServer: {
+        host: "0.0.0.0",
+        port: 8080, //port that we're using for local host (localhost:8080)
+        static: path.resolve(appDirectory, "public"), //tells webpack to serve from the public folder
+        hot: true,
+        devMiddleware: {
+            publicPath: "/",
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.wgsl$/,
+                use: 'ts-shader-loader'
+            }
+        ]
+        
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: "index.html"
+        })
+    ],
+    mode: "development",
+};
