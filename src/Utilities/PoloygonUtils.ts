65254de75758092fa3223b041cbce05aaee97d51
greg k <gpkort@gmail.com>
2023-05-11 08:33:49 -0600

    poly fix

--- src/Utilities/PoloygonUtils.ts
+++ src/Utilities/PoloygonUtils.ts
@@ -4,20 +4,27 @@ import { IEquallateral } from "../geometry/Polygon";
 
 export const getVertices = (shape: IEquallateral) => {
   const sides = shape.numberOfSides;
-  const verticies = Array<vec2>(sides);
-  const theta = (2 * Math.PI) / sides / 2;
-  const rad = getRadiux(shape.sideLength / 2, theta);
+  const verticies = new Array<vec2>();
+  const alpha = (2 * Math.PI) / sides;
+  const radiux = getRadiux(shape.sideLength, alpha);
+  let theta = Math.PI / 2
 
   for (let i = 0; i < sides; i++) {
-    // point = i === 0 ? getNextVertex(point, Math.PI /2, );
-    // verticies.push(point);
+    verticies.push(getVertix(theta + (alpha * i), radiux, shape.center))
   }
+
+  return verticies;
 };
 
-const getVertix = (center: vec2, theta: number, length: number) => {
-  const height = 0;
+const getVertix = (theta: number, radiux: number, center: vec2) => {
+  //console.log(`t: ${theta}, r: ${radiux}, cx: ${center[0]}, cy: ${center[1]}`)
+  const x = center[0] + radiux * Math.cos(theta);
+  const y = center[1] - radiux * Math.sin(theta);
+  console.log(`x: ${x}, y: ${y}`)
+  return vec2.fromValues(x, y)
 };
 
 const getRadiux = (length: number, theta: number) => {
-  return (length / Math.sin(theta)) * Math.sign(theta);
+  const beta = ((2 * Math.PI) - theta) / 2;
+  return (length / Math.sin(theta)) * Math.sign(beta);
 };
--- src/Utilities/PolygonUtils.test.ts
+++ src/Utilities/PolygonUtils.test.ts
@@ -1,9 +1,37 @@
 import { describe, expect, test } from "vitest";
-import { originTpointXform } from ".";
+import { getVertices } from ".";
 import { vec2 } from "gl-matrix";
+import { IEquallateral } from "../geometry/Polygon";
 
 describe("xform", () => {
   test("t1", () => {
-    expect(true).toBeTruthy();
+    const square: IEquallateral = {
+      numberOfSides: 4,
+      sideLength: 1,
+      outline: {
+        width: 1,
+        lineStyle: [],
+        color: {
+          r: 0,
+          g: 0,
+          b: 0,
+          a: 100
+        } 
+      },
+      fillColor: {
+        r: 0,
+        g: 0,
+        b: 0,
+        a: 100
+      },
+      center: vec2.fromValues(0, 0)
+    };
+    const verticies = getVertices(square);
+    console.log(`x: ${verticies[0][0]}, y: ${verticies[0][1]}`)
+
+    expect(vec2.equals(verticies[0], vec2.fromValues(0, -1))).toBeTruthy();
+    expect(vec2.equals(verticies[1], vec2.fromValues(-1, 0))).toBeTruthy();
+    expect(vec2.equals(verticies[2], vec2.fromValues(0, -1))).toBeTruthy();
+    expect(vec2.equals(verticies[3], vec2.fromValues(0, 1))).toBeTruthy();
   });
 });
