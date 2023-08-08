f997e82af27be9836f0011a7aba030aa5e1eab97
Greg Korthuis <greg.korthuis@medtronic.com>
2023-08-08 08:36:40 -0600

    added tests

--- src/Utilities/TwoD/Camera2D.test.ts
+++ src/Utilities/TwoD/Camera2D.test.ts
@@ -1,4 +1,4 @@
-import { mat3, vec2 } from "gl-matrix";
+import { mat2d, mat3, vec2, vec3 } from "gl-matrix";
 import { assertType, describe, expect, test } from "vitest";
 
 describe("transformation", () => {
@@ -10,6 +10,13 @@ describe("transformation", () => {
       [0, 0, 1],
       [2, 3, 0],
     ];
+
+    const point1 = mat2d.fromValues(1, 0, 0, 1, 5, 2);
+    const point2 = vec2.fromValues(2, 3);
+    let res = mat2d.create();
+
+    mat2d.translate(res, point1, point2);
+    console.log(`res = ${res}`);
 
     expect(affine.length).toEqual(4);
   });
