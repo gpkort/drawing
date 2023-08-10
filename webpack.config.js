11c7f981afbb9e6f1302fd8afb3e70de84f814fc
greg k <gpkort@gmail.com>
2023-08-09 19:51:58 -0600

    added template

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,47 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const fs = require('file-system')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const fs = require("file-system");
 const appDirectory = fs.realpathSync(process.cwd());
 
 module.exports = {
-    mode: 'development',
-    entry: {
-        index: './src/index.ts'
-      },
-    plugins: [
-        new HtmlWebpackPlugin({ 
-          title: 'Development',  
-          inject: true,}),
-    ],
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'public'),
-        clean: true,
+  mode: "development",
+  entry: {
+    index: "./src/index.ts",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Development",
+      template: "src/index.html",
+      inject: true,
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "public"),
+    clean: true,
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  devServer: {
+    host: "0.0.0.0",
+    port: 8080, //port that we're using for local host (localhost:8080)
+    static: path.resolve(appDirectory, "public"), //tells webpack to serve from the public folder
+    hot: true,
+    devMiddleware: {
+      publicPath: "/",
     },
-    resolve: {
-      extensions: [".tsx", ".ts", ".js"],
   },
-    devServer: {
-      host: "0.0.0.0",
-      port: 8080, //port that we're using for local host (localhost:8080)
-      static: path.resolve(appDirectory, "public"), //tells webpack to serve from the public folder
-      hot: true,
-      devMiddleware: {
-          publicPath: "/",
-      }
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
   },
-    module: {
-        rules: [
-          {
-            test: /\.tsx?$/,
-            use: 'ts-loader',
-            exclude: /node_modules/,
-          },
-        ],
-      },
-      resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-      },
-      
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
 };
